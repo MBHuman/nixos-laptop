40ea01d45fa74f54b41105b1c2a2c304ff19a297
MBHuman <dmitrievevgeny57@gmail.com>
2026-05-08 06:33:47 +0300

    Fix

--- flakes/configuration.nix
+++ flakes/configuration.nix
@@ -75,7 +75,9 @@
   # 6. FONTS (Nerd Fonts required for agnoster theme)
   # ==========================================
   fonts.packages = with pkgs; [
-    (nerdfonts.override { fonts = [ "Meslo" "FiraCode" "JetBrainsMono" ]; })
+    nerd-fonts.meslo-lg
+    nerd-fonts.fira-code
+    nerd-fonts.jetbrains-mono
   ];
 
   # ==========================================
